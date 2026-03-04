894cb01723aca97158ae79096b4967b3970929c7
adgonzalez2018 <71677050+Adgonzalez2018@users.noreply.github.com>
2026-03-04 13:57:01 -0500

    test?

--- client/src/pages/Connect/Imports.js
+++ client/src/pages/Connect/Imports.js
@@ -3,7 +3,7 @@ import "../Auth/Auth.css";
 
 import React, { useState, useRef, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { useAuth } from "../../hooks/useAuth";
+import { useAuth  } from "../../hooks/useAuth";
 
 // Assets
 import personImg from "../../assets/images/Fargo_person.png";
