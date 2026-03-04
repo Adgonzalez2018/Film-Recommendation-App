5545539e5259ce808c4037a59706a12f02dbc807
adgonzalez2018 <71677050+Adgonzalez2018@users.noreply.github.com>
2026-03-04 15:02:44 -0500

    added redirection thru routes.jsx

--- client/src/routes.jsx
+++ client/src/routes.jsx
@@ -1,4 +1,6 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
+import { useAuth } from "./hooks/useAuth";
+
 import LandingPage from "./pages/Landing/LandingPage";
 
 import Register from "./pages/Auth/Register";
@@ -13,6 +15,21 @@ import WeeklyStats from "./pages/Stats/WeeklyStats"
 import DirectoryStats from "./pages/Stats/DirectoryStats"
 import AllStats from "./pages/Stats/AllStats"
 
+function RequireAuth({ children }){
+  const { accessToken, isAuthenticating } = useAuth();
+  if (isAuthenticating) return null;
+  if (!accessToken) return <Navigate to="/signin" replace />;
+  return children;
+}
+
+function RequireOnboarding({ children }){
+  const { isOnboarded, isAuthenticating } = useAuth();
+  if (isAuthenticating) return null;
+  // if auth is true but onboarding unknown, keep waiting
+  if (isOnboarded == null) return null;
+  if (!isOnboarded) return <Navigte to="/import" replace/>;
+  return children;
+}
 
 
 export default function AppRoutes() {
@@ -24,12 +41,28 @@ export default function AppRoutes() {
       <Route path="/signin" element={<SignIn />} />
       <Route path="/profile" element={<Profile/>}/>
 
-      <Route path="/import" element={<Imports />} />
+      <Route path="/import" 
+      element={
+        <RequireAuth>
+          <Imports />          
+        </RequireAuth>
+    } 
+      />
 
 
-      <Route path="/chat" element={<Chat />} />
+      <Route path="/chat" element={
+        <RequireOnboarding>
+          <Chat /> 
+        </RequireOnboarding>
+      } 
+        />
 
-      <Route path="/stats" element={<DirectoryStats />} />
+      <Route path="/stats" element={
+        <RequireOnboarding>
+          <DirectoryStats />
+        </RequireOnboarding>
+        } 
+        />
       <Route path="/stats/weekly" element={<WeeklyStats />} />
       <Route path="/stats/alltime" element={<AllStats />} />
 
