d94236bc6b3289b65bb3e42a986a7f41c205ca93
adgonzalez2018 <71677050+Adgonzalez2018@users.noreply.github.com>
2026-04-01 16:00:27 -0400

    command for deleting super user update

--- server/filmrec/api/management/commands/delete_superuser.py
+++ server/filmrec/api/management/commands/delete_superuser.py
@@ -1,30 +1,30 @@
 from django.contrib.auth import get_user_model
+from django.core.management.base import BaseCommand
 from api.models import WatchEvent, MovieUser, FilmBank, ImportBatch
 
 User = get_user_model()
 
-def reset_user_app_date(user):
-    WatchEvent.objects.filter(user=user).delete()
-    MovieUser.objects.filter(user=user).delete()
-    FilmBank.objects.filter(user=user).delete()
-    ImportBatch.objects.filter(user=user).delete()
+class Command(BaseCommand):
+    help = "Reset all app data for a user"
+    def add_arguments(self, parser):
+        parser.add_argument("--username", type=str, required=True)
+    def handle(self, *args, **opts):
+        user = User.objects.get(username=opts["username"])
 
-    user.manual_import_count = 0
-    user.rss_import_count = 0
-    user.last_sync = None
-    user.last_manual_sync = None
-    user.last_rss_sync = None
-    user.last_rss_account_switch = None
-    user.letterboxd_username = ""
-    user.taste_vector_store_id = None
-    user.has_skipped_onboarding = False
-    user.save(update_fields=[
-        "manual_import_count",
-        "rss_import_count",
-        "last_sync",
-        "last_manual_sync",
-        "last_rss_sync",
-        "last_rss_account_switch",
-        "letterboxd_username",
-        "taste_vector_store_id",
-    ])
+        WatchEvent.objects.filter(user=user).delete()
+        MovieUser.objects.filter(user=user).delete()
+        FilmBank.objects.filter(user=user).delete()
+        ImportBatch.objects.filter(user=user).delete()
+
+        user.manual_import_count = 0
+        user.rss_import_count = 0
+        user.last_sync = None
+        user.last_manual_sync = None
+        user.last_rss_sync = None
+        user.last_rss_account_switch = None
+        user.letterboxd_username = ""
+        user.taste_vector_store_id = None
+        user.has_skipped_onboarding = False
+        user.save()
+
+        self.stdout.write(self.style.SUCCESS(f"Reset data for {user.username}"))
