c7e2064edeb5883f22636939699c9aa330fc41b1
adgonzalez2018 <71677050+Adgonzalez2018@users.noreply.github.com>
2026-04-02 15:05:50 -0400

    update film recommender version

--- client/src/pages/Chat/Chat.js
+++ client/src/pages/Chat/Chat.js
@@ -364,7 +364,7 @@ const Chat = () => {
 
       <main className="chat-main">
         <header className="chat-header">
-          <h1 className="header-title">Film-Recommender v1.0</h1>
+          <h1 className="header-title">Film-Recommender v1.1</h1>
         </header>
 
         <div className="messages-container">
